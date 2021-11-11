0961c166fb2a2bdbe53d8db5fbaf129196f6dd3a
Prateek <prateek.chauhan0194@gmail.com>
2021-11-11 23:58:49 +1100

    Sample request

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const connectMongo = require('./db');
 connectMongo();
+
+const express = require('express')
+const app = express()
+const port = 3333
+
+app.get('/', (req, res) => {
+    res.send('This is asset management app backend.');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
