09774bc149da92abff4ee15c5177ae6988aa3494
Prateek <prateek.chauhan0194@gmail.com>
2021-11-12 00:06:36 +1100

    Added a loan schema to store data of loaned assets

---
+++ models/Loans.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const loansSchema = new Schema({
+    serviceNumber: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    rank: {
+        type: String,
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    department: {
+        type: String,
+        required: true,
+    },
+    itemName: {
+        type: String,
+        required: true,
+    },
+    itemSerialNumber: {
+        type: String,
+        required: true,
+    },
+    itemModel: {
+        type: String,
+        required: true,
+    },
+    dateOfIssue: {
+        type: Date,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Loans', loansSchema);
