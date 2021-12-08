40345de0b9afc780c37936e35ed20dabc0394a58
Prateek <prateek.chauhan0194@gmail.com>
2021-12-09 10:46:48 +1100

    Added an unauthenticated test route

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use('/api/v1/borrower', require('./routes/borrower'));
 app.use('/api/v1/item', require('./routes/item'));
 app.use('/api/v1/auth', require('./routes/auth'));
 
+// Hello world route
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
